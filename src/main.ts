fb0a0e7dc8af43fd6cb2354869aefa22c7b5413d
Roberto Arenas <roberto.arenas@soacmc.com>
2022-12-23 16:10:12 -0600

    adapting project and customize cors to enable localhost:3000 - react

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.enableCors({
+    // Enable cors for React
+    origin: ['http://localhost:3000'],
+  });
+  await app.listen(8000);
 }
 bootstrap();
